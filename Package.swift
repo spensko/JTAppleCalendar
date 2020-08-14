a33dd34d92c71b59e42cfd6cd638c7d98983fcf9
Mikhail Serov <iteron@gmail.com>
2020-08-14 18:22:06 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,23 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-
 let package = Package(
-    name: "JTAppleCalendar"
+    name: "JTAppleCalendar",
+    platforms: [
+        .iOS(.v9),
+    ],
+    products: [
+        .library(
+            name: "JTAppleCalendar",
+            targets: ["JTAppleCalendar"]),
+    ],
+    targets: [
+        .target(
+            name: "JTAppleCalendar",
+            dependencies: []),
+        .testTarget(
+            name: "JTAppleCalendarTests",
+            dependencies: ["JTAppleCalendar"]),
+    ]
 )
